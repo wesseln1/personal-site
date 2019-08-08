44bdd6c7f5ae412eb5106ca026c45c9c1db536f1
Steve Brownlee <chortlehoort@gmail.com>
2019-08-08 12:27:39 -0500

    Also adding a message to the DOM when app starts

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,5 @@
-console.log("Your Webpack application is set up and ready to go. Please start writing code.")
+const message = "Your Webpack application is set up and ready to go. Please start writing code."
+
+document.querySelector("#container").innerHTML = `<h1>${message}</h1>`
+
+console.log(message)
